dcaa8e26d6cf06f1c6a9050559d5a1ad7e5a4836
eshy5001 <specialkurukuru@gmail.com>
2021-03-06 12:55:57 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,35 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,  presence: true, uniqueness: { case_sensitive: true }
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+    validates :last_name
+    validates :last_name_kana
+    validates :first_name
+    validates :first_name_kana
+    validates :birth_date
+  end
+
+  with_options uniqueness: { case_sensitive: true } do
+    validates :nickname
+    validates :email
+  end
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email,     presence: true, uniqueness: { case_sensitive: true }, format: { with: VALID_EMAIL_REGEX }
+  validates :email,     format: { with: VALID_EMAIL_REGEX }
 
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password,  presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
-
-  validates :last_name,       presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana,  presence: true,  format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name,      presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date,      presence: true
+  validates :password,  length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
 end
